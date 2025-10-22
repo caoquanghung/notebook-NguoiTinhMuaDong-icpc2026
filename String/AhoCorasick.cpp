6aaf1560c843f46f1d76d3a14c293e1dbad90e20
Hung <caoquanghungne@gmail.com>
2025-10-22 21:57:23 +0700

    Change Aho Template
    Coan's aho

--- String/AhoCorasick.cpp
+++ String/AhoCorasick.cpp
@@ -1,102 +1,87 @@
-// Tested:
-// - https://open.kattis.com/problems/stringmultimatching
-// - https://icpc.kattis.com/problems/firstofhername
-// - https://oj.vnoi.info/problem/binpal
-//
-// Notes:
-// - Node IDs from 0 to aho.sz.
-// - Characters should be normalized to [0, MC-1].
-// - For each node of AhoCorasick, we store a linked list containing all queries "associated" with this node.
-//   The reason is that, when we reach a node in AhoCorasick, it's possible to match several queries at once.
-//   (this happens when queries are suffix of others, e.g. C, BC, ABC).
-//   This also means 1 node maps to several queries, and 1 query maps to several nodes.
-//   However I believe that the sum of length of all linked list is O(N) -- TODO: Source / proof required.
+/*
+    Aho-Corasick string matching algorithm with exit links and occurrence counting
+    Uses a tree structure to efficiently count occurrences of patterns
+    in a given text after multiple updates.
 
-const int MN = 1000111; // MN > total length of all patterns
-const int MC = 26; // Alphabet size.
-
-// Start of Linked list
-struct Node {
-    int x; Node *next;
-} *nil;
-struct List {
-    Node *first, *last;
-    List() { first = last = nil; }
-    void add(int x) {
-        Node *p = new Node;
-        p->x = x; p->next = nil;
-        if (first == nil) last = first = p;
-        else last->next = p, last = p;
+    nxt[c]: transition for character c.
+    First nxt is the trie transition. After aho build, it becomes the automaton transition.
+    suffixLink: longest proper suffix of the current node that is also a prefix in the trie
+    exitLink: longest suffix that is an end of some inserted string (for counting occurrences)
+*/
+struct AhoCorasick{
+private:
+  struct TrieNode {
+    int nxt[26];
+    int exitLink;
+    int suffixLink;
+    int cnt;
+    
+    TrieNode() : exitLink(-1), suffixLink(-1), cnt(0) {
+      fill(nxt, nxt + 26, -1);
     }
-};
-// End of linked list
-//
-struct Aho {
-    int qu[MN], suffixLink[MN];
-    List leaf[MN];
-    int link[MN][MC];
-    int sz;
-    bool calledBuildLink;
-
-    void init() {
-        calledBuildLink = false;
-        sz = 0;
-        memset(suffixLink, 0, sizeof suffixLink);
-        leaf[0] = List();
-        memset(link[0], -1, sizeof link[0]);
-    }
-    int getChild(int type, int v, int c) {
-        if (type == 2) assert(calledBuildLink);
-
-        if (link[v][c] >= 0) return link[v][c];
-        if (type == 1) return 0;
-        if (!v) return link[v][c] = 0;
-        return link[v][c] = getChild(type, suffixLink[v], c);
-    }
-    void buildLink() {
-        calledBuildLink = true;
-        int first, last;
-        qu[first = last = 1] = 0;
-        while (first <= last) {
-            int u = qu[first++];
-            REP(c,MC) {
-                int v = link[u][c]; if (v < 0) continue;
-                qu[++last] = v;
-                if (u == 0) suffixLink[v] = 0;
-                else suffixLink[v] = getChild(2, suffixLink[u], c);
-
-                if (leaf[suffixLink[v]].first != nil) {
-                    if (leaf[v].first == nil) {
-                        leaf[v].first = leaf[suffixLink[v]].first;
-                        leaf[v].last = leaf[suffixLink[v]].last;
-                    }
-                    else {
-                        leaf[v].last->next = leaf[suffixLink[v]].first;
-                        leaf[v].last = leaf[suffixLink[v]].last;
-                    }
-                }
-            }
+  };
+ 
+  vector<TrieNode> trie;
+  Tree& tree; // This is the exit tree for counting occurrences
+  
+public:
+  AhoCorasick(Tree& _tree) : tree(_tree) {
+    trie.push_back(TrieNode());
+  }
+ 
+  vector<int> insert_string(const string& s) {
+    int p = 0;
+    vector<int> ans = {p};
+    for (char c : s) {
+      c -= 'a';
+      if (trie[p].nxt[c] == -1) {
+        trie[p].nxt[c] = trie.size();
+        trie.push_back(TrieNode());
+      }
+      p = trie[p].nxt[c];
+      ans.push_back(p);
+    }  
+    ++trie[p].cnt;
+    return ans;
+  }
+ 
+  void buildAho() {
+    queue<int> q;
+    q.push(0);
+    while(!q.empty()) {
+      int v = q.front(); q.pop();
+      int sfLink = trie[v].suffixLink;
+      if (v != 0) {
+        trie[v].exitLink = (trie[sfLink].cnt > 0) ? sfLink : trie[sfLink].exitLink;
+        if (trie[v].cnt > 0) {
+          // build exit tree
+          tree.addEdge(trie[v].exitLink == -1 ? 0 : trie[v].exitLink, v); 
         }
+      }
+ 
+      FOR(c, 26) {
+        int &_nxt = trie[v].nxt[c];
+        int nxtsf = (v == 0) ? 0 : trie[sfLink].nxt[c];
+        if (_nxt == -1) { 
+          _nxt = nxtsf;
+        }
+        else {  
+          trie[_nxt].suffixLink = nxtsf;
+          q.push(_nxt);
+        }
+      } 
     }
-} aho;
-// Usage:
-aho.init(); // Initialize
-// Foreach query, insert one character at a time:
-            int p = 0;
-            while (k--) {
-                int x; scanf("%d", &x);
-                int t = aho.getChild(1, p, x);
-                if (t > 0) p = t;
-                else {
-                    ++aho.sz;
-                    aho.leaf[aho.sz] = List();
-                    memset(aho.link[aho.sz], -1, sizeof aho.link[aho.sz]);
-
-                    aho.link[p][x] = aho.sz;
-                    p = aho.sz;
-                }
-            }
-            aho.leaf[p].add(i);
-// Init back link
-        aho.buildLink();
-// After this stage, we should use aho.getChild(2, node, c) to jump
+ 
+    tree.initDfs(0);
+  }
+ 
+  void update(int node) {
+    node = (trie[node].cnt > 0) ? node : trie[node].exitLink;
+    if (node > 0) tree.updateToRoot(node, +1);
+  }
+ 
+  int getApp(int node) {
+    return tree.getSubTree(node);
+  }
+ 
+} Aho(globalTree);
